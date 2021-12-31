c30f59e45977dcdb59987ffded56929420c953df
hossem_ubuntu <hammamihossem99@gmail.com>
2021-12-31 03:49:01 +0100

    add notification globally

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -1,11 +1,13 @@
 import { SessionProvider } from "next-auth/react";
 import { ToastContextProvider } from "../components/Notification/context/notification/notifContext";
 import "../styles/globals.css";
+import { Notification } from "../components";
 
 function MyApp({ Component, pageProps: { session, ...pageProps } }) {
   return (
     <SessionProvider session={session}>
       <ToastContextProvider>
+        <Notification />
         <Component {...pageProps} />
       </ToastContextProvider>
     </SessionProvider>
