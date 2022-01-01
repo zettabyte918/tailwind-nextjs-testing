10e6704336c80772535ee988a53a8fbd5349dbfd
hossem_ubuntu <hammamihossem99@gmail.com>
2022-01-01 18:11:27 +0100

    add nprogress bar loader

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -2,6 +2,15 @@ import { SessionProvider } from "next-auth/react";
 import { ToastContextProvider } from "../components/Notification/context/notification/notifContext";
 import "../styles/globals.css";
 import { Notification } from "../components";
+import Router from "next/router";
+import NProgress from "nprogress";
+
+Router.events.on("routeChangeStart", (url) => {
+  console.log(`Loading: ${url}`);
+  NProgress.start();
+});
+Router.events.on("routeChangeComplete", () => NProgress.done());
+Router.events.on("routeChangeError", () => NProgress.done());
 
 function MyApp({ Component, pageProps: { session, ...pageProps } }) {
   return (
