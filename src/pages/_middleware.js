f7f4fc378b20780f1febc6e6b4fdcd4f35cede4e
hossem_ubuntu <hammamihossem99@gmail.com>
2022-01-01 20:00:04 +0100

    configure middleware for authetification

---
+++ src/pages/_middleware.js
@@ -0,0 +1,16 @@
+import { getToken } from "next-auth/jwt";
+import { NextResponse } from "next/server";
+
+async function middleware(req, ev) {
+  const session = await getToken({ req, secret: process.env.SECRET });
+  const { pathname } = req.nextUrl;
+
+  if (session && pathname === "/auth/signin") return NextResponse.redirect("/");
+
+  if (!session && pathname !== "/auth/signin")
+    return NextResponse.redirect("/auth/signin");
+
+  if (session || pathname.includes("/api/auth")) return NextResponse.next();
+}
+
+export default middleware;
