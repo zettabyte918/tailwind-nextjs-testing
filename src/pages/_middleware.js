c8c767b832610ddbdc54aeea42cdadf39389e3f4
hossem_ubuntu <hammamihossem99@gmail.com>
2022-01-07 16:50:36 +0100

    fix pwa

--- src/pages/_middleware.js
+++ src/pages/_middleware.js
@@ -6,6 +6,8 @@ async function middleware(req, ev) {
   const { pathname } = req.nextUrl;
   if (session && pathname === "/auth/signin") return NextResponse.redirect("/");
 
+  if (pathname.includes("/manifest.json")) return NextResponse.next();
+
   if (session || pathname.includes("/api/auth")) return NextResponse.next();
 
   if (!session && pathname !== "/auth/signin")
